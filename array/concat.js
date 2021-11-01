06192285b71ddde443c88d2bfcc0f1be8bffbbe7
Marko <marcoanunesjr@gmail.com>
2021-11-01 18:20:44 -0300

    confirmar

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Waleskah' ,  'Cibalena'];
+const filhos = ['Uoxinton' ,  'Uesclei' ];
+const totdos = filhas.concat(filhos);
+console.log(totdos , filhas , filhos);
+
+console.log([].concat([1 , 2], [3, 4], 5, [[6, 7]]))
