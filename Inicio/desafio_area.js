54f571ece7dc02a7dce133bcf72dc87d62fced25
Marko <marcoanunesjr@gmail.com>
2021-11-01 16:10:31 -0300

    confirmar

---
+++ Inicio/desafio_area.js
@@ -0,0 +1,10 @@
+// PI 3.141592
+// PI * raio * raio
+
+let raio = 10;
+const PI = 3.141592;
+let area = PI * raio * raio;
+
+console.log('O resultado da é', area, "m²");
+
+console.log(Math.PI); 
---
+++ Inicio/desafio_troca.js
@@ -0,0 +1,18 @@
+let a = 7;
+let b = 94;
+
+console.log(a);
+console.log(b);
+
+//a = 94;
+//b = 7;
+
+// let temp = a;
+// a = b;
+// b = temp;
+
+[a,b] = [b,a];
+
+console.log(a);
+console.log(b);
+
