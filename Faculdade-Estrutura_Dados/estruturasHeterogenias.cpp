6fad695c90a7a18762b7dc8e289b21c304192d51
Marko <marcoanunesjr@gmail.com>
2021-12-14 07:32:43 -0300

    ok

--- Faculdade-Estrutura_Dados/estruturasHeterogenias.cpp
+++ Faculdade-Estrutura_Dados/estruturasHeterogenias.cpp
@@ -5,19 +5,42 @@
 
 using namespace std;
 
+struct Coordenada
+{
+    int x, y, z;
+};
+
 int main()
 {
     setlocale(LC_ALL, "Portugueses");
 
-    int x, y, z;
+    Coordenada pontoA;
+    Coordenada pontoB;
+    Coordenada pontoC;
 
-    x = 10;
-    y = 20;
-    z = 50;
+    pontoA.x = 10;
+    pontoA.y = 20;
+    pontoA.z = 50;
 
-    cout << "Coordenada x: " << x << endl;
-    cout << "Coordenada y: " << y << endl;
-    cout << "Coordenada z: " << z << endl;
+    cout << "Coordenada x: " << pontoA.x << endl;
+    cout << "Coordenada y: " << pontoA.y << endl;
+    cout << "Coordenada z: " << pontoA.z << endl;
+
+    pontoB.x = 100;
+    pontoB.y = 200;
+    pontoB.z = 500;
+
+    cout << "Coordenada x: " << pontoB.x << endl;
+    cout << "Coordenada y: " << pontoB.y << endl;
+    cout << "Coordenada z: " << pontoB.z << endl;
+
+    pontoC.x = 16;
+    pontoC.y = 25;
+    pontoC.z = 57;
+
+    cout << "Coordenada x: " << pontoC.x << endl;
+    cout << "Coordenada y: " << pontoC.y << endl;
+    cout << "Coordenada z: " << pontoC.z << endl;
 
     system("pause");
     return 0;
